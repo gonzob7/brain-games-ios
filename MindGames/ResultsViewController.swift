f22276e4c645639998590ce88336f2826d18c0f3
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-11 13:20:46 -0800

    added resultsviewcontroller class

---
+++ MindGames/ResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsViewController.swift
+//  MindGames
+//
+//  Created by Gonzalo Birrueta on 12/11/19.
+//  Copyright © 2019 Gonzalo Birrueta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsViewController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
